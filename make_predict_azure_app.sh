c3f2cfdee227a5a2fe33e526ecdb79871e17cb5b
Gabriel Estivalet <gestivalet@gmail.com>
2023-02-26 16:23:31 +0000

    add make_predict_azure_app.sh file

---
+++ make_predict_azure_app.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+PORT=443
+echo "Port: $PORT"
+
+# POST method predict
+curl -d '{  
+   "sepal length (cm)":{  
+      "0":7.2
+   },
+   "sepal width (cm)":{  
+      "0":3.6
+   },
+   "petal length (cm)":{  
+      "0":6.1
+   },
+   "petal width (cm)":{  
+      "0":2.5
+   }
+}'\
+     -H "Content-Type: application/json" \
+     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     #your application name <yourappname>goes here
